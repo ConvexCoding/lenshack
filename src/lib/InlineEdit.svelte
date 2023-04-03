6d599e006ab925a74cf11dbabdc81119a90b28a7
ConvexCoding <64615930+glherrit@users.noreply.github.com>
2023-04-03 17:50:07 -0400

    add some text and equations

---
+++ src/lib/InlineEdit.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let value: string;
+	export let placeholder: string;
+
+	const debug = false;
+
+	let inputEl: HTMLInputElement;
+	let editing = false;
+
+	// trick to set focus once it binds
+	$: inputEl && inputEl.focus();
+</script>
+
+{#if editing}
+	<input
+		bind:this={inputEl}
+		class="p-0 text-center"
+		type="text"
+		style:width={`calc(${value.length}*var(--math-text-size))`}
+		style:min-width={`calc(2*var(--math-text-size))`}
+		{placeholder}
+		bind:value
+		on:blur={() => (editing = false)}
+	/>
+{:else}
+	<button
+		class="w-min rounded-token hover:bg-black/10 {debug ? 'bg-black/10' : ''}"
+		on:click={() => (editing = true)}
+		on:focus={() => (editing = true)}
+	>
+		{value || placeholder}
+	</button>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,58 @@
 <script lang="ts">
   import { Canvas } from '@threlte/core'
-  //import Scene from './Scene.svelte';
-  import Scene1 from './Scene1.svelte'
-  import Scene2 from './Scene2.svelte'
-  import Scene3 from './Scene3.svelte'
+  import { MirroredRepeatWrapping } from 'three'
+  import Gauss from './Equations/Gauss.svelte'
+  import GaussProp from './Equations/GaussProp.svelte'
+  import Rayleigh from './Equations/Rayleigh.svelte'
+  import Scene from './Scenes/Scene.svelte'
+  import Scene1 from './Scenes/Scene1.svelte'
+  import Scene2 from './Scenes/Scene2.svelte'
+  import Scene3 from './Scenes/Scene3.svelte'
 </script>
 
 <div class="outercontainer flex h-full flex-col overflow-y-auto pb-10">
-  <h1 class="title">Welcome to Skeleton!</h1>
-  <h2>LLS #6: Evaluating Lens Design</h2>
+  <h1 class="title flex justify-center">Gaussian Laser Beam Blog</h1>
   <br />
-  When designing a lens or choosing a lens, it's important to know if the lens is good enough for your
-  application. Having a suite of analysis types is an important part of this process. In this post we
-  will cover five analysis types that can be used to evaluate lens performance.
+  <h3 class="flex justify-center text-orange-500">Gaussian Beams</h3>
+  <h3 class="flex justify-center text-orange-500">&</h3>
+  <h3 class="flex justify-center text-orange-500">
+    How They Are Focused by the Ideal Laser Lenses
+  </h3>
+  <br />
+  <br />
+  <div class="ml-5 mr-5 border-blue-500">
+    <p>
+      Laser beams in general have been made popular to the general public by many of the science
+      fiction movies of the past decades. In the real world one the most common types of laser beams
+      is the Gaussian laser beam. Gaussian laser beams are a common tool in the laboratory and in
+      industry. They are used in a variety of applications from eye surgery, laser drilling of
+      circuit boards, and laser cutting of almost all know materials. Gaussian beam propagate (like
+      all photons) in a straight line.
+    </p>
+    <br />
+    <p>
+      Although laser beams are commonly thought to be collimated light, this is not the case. The
+      beams propagate according to the following equation:
+    </p>
+    <br />
+    <GaussProp />
+    <br />
+    <p>
+      In this equation w(z) is the beam waist (or radius) at any given distance z - the distance the
+      laser beam travels along it's optic axis. The value z<sub>r</sub>
+      is the Rayleigh Range, which is a constant for a gaussian laser beam of wavelength, λ, and initial
+      beam radius of w<sub>0</sub>. The equation for the Rayleigh Range constant is:
+    </p>
+    <br />
+    <Rayleigh />
+    <br />
+    Maybe graphic here showing prop of laser beam based on starting waist.
+    <br />
+    next graphic show laser beam through Lenses and some text about Focused
+    <br />
+
+    <Gauss />
+  </div>
   <br />
   <br />
   <div class="container h-2/3 w-2/3 border-8 border-yellow-500 bg-white">
@@ -21,12 +61,7 @@
     </Canvas>
   </div>
   <br />
-  Ray Tracing Arguably one of the simplest forms of performance analysis is done by looking at the ray
-  trace through a lens. It can reveal, in rather crude terms, if the lens is focusing the rays to a point
-  (assuming we are evaluating a simple focusing lens). Figure 1 shows ray traces for two short focal
-  length lenses. Figure 1a shows a simple plano convex lens and Figure 1b shows this same lens but with
-  aspheric terms to correct spherical aberrations. Note how the rays in 1a fail to converge to a single
-  point, but the rays in 1b do.
+  more text
   <br />
   <br />
   <div class="container h-2/3 w-2/3 border-8 border-red-500 bg-white">
